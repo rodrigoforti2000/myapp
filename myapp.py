e06d842e96500b07cc6ac59ba0625a4920229c65
rodrigoforti2000 <67616733+rodrigoforti2000@users.noreply.github.com>
2020-07-30 13:13:31 -0300

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+st.write("""
+# Essa é minha primeira aplicação web
+
+### Júlia, oq vc acho??
+""")
+
+age = st.selectbox("Nota", ["ruim","muito boa","Vsf e vai fazer alguma coisa útil"])
