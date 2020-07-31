36aba7eea19f898d75eaedbe6d6bc2628a9d16d4
rodrigoforti2000 <67616733+rodrigoforti2000@users.noreply.github.com>
2020-07-30 22:42:04 -0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 import streamlit as st
@@ -14,55 +13,16 @@ pokemon = pd.read_csv("https://gist.githubusercontent.com/armgilles/194bcff35001
 pallete_pok = {False:"lightgrey",
                True:"#1F77B4"}
 
-add_selectbox = st.sidebar.selectbox(
-    "Escolha um:",
-    ("Defense", "Speed")
+plot_varx = st.sidebar.selectbox(
+    "Eixo X:",
+    ("Defense", "Speed","Attack","HP","Total")
 )
 
-if(add_selectbox == "Defense"):
-    sns.scatterplot(x = "Defense", y = "Attack", data = pokemon, hue = "Legendary", palette = pallete_pok)
-    plt.title("Ataque x Defesa\nseparado por lendários", loc = "left")
-    st.pyplot()
-else:
-    sns.scatterplot(x="Speed", y="Attack", data=pokemon, hue="Legendary", palette=pallete_pok)
-    plt.title("Ataque x Defesa\nseparado por lendários", loc="left")
-    st.pyplot()
+plot_vary = st.sidebar.selectbox(
+    "Eixo Y:",
+    ("Defense", "Speed","Attack","HP","Total")
+)
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.deck_gl_chart(
-     viewport={
-         'latitude': 37.76,
-         'longitude': -122.4,
-         'zoom': 11,
-         'pitch': 50,
-     },
-     layers=[{
-         'type': 'HexagonLayer',
-         'data': df,
-         'radius': 200,
-         'elevationScale': 4,
-         'elevationRange': [0, 1000],
-         'pickable': True,
-         'extruded': True,
-     }, {
-         'type': 'ScatterplotLayer',
-         'data': df,
-     }])
-
-genre = st.radio(
-     "What's your favorite movie genre",
-     ('Comedy', 'Drama', 'Documentary'))
-
-if genre == 'Comedy':
-     st.write('You selected comedy.')
-else:
-     st.write("You didn't select comedy.")
-
-
-agree = st.checkbox('I agree')
-
-if agree:
-    st.write('Great!')
+sns.scatterplot(x = plot_varx, y = plot_vary, data = pokemon, hue = "Legendary", palette = pallete_pok)
+plt.title(plot_vary +" x "+ plot_varx +"\nseparado por lendários", loc = "left")
+st.pyplot()
