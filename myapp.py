dccd26e64f6305391e082865ccd266e79d83522e
rodrigoforti2000 <67616733+rodrigoforti2000@users.noreply.github.com>
2020-07-30 22:23:35 -0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,17 +1,68 @@
+
 import pandas as pd
 import numpy as np
 import streamlit as st
+import matplotlib.pyplot as plt
+import  seaborn as sns
 
-st.write("""
-# Minha primeira aplicação web
 
-### Júlia, o que vc achou??
-""")
+st.title("Pokemon Dataset")
 
-age = st.selectbox("Opinião", ["ruim","muito boa","Vsf e vai fazer alguma coisa útil"])
+pokemon = pd.read_csv("https://gist.githubusercontent.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6/raw/92200bc0a673d5ce2110aaad4544ed6c4010f687/pokemon.csv")
 
-st.write("""
-### O comuninsmo vencerá?
-""")
+#mudando type de uma coluna
+pallete_pok = {False:"lightgrey",
+               True:"#1F77B4"}
 
-communism = st.selectbox("Opinião",["Não","Sim","Com certeza"])
+add_selectbox = st.sidebar.selectbox(
+    "Escolha um:",
+    ("Defense", "Speed")
+)
+
+if(add_selectbox == "Defense"):
+    sns.scatterplot(x = "Defense", y = "Attack", data = pokemon, hue = "Legendary", palette = pallete_pok)
+    plt.title("Ataque x Defesa\nseparado por lendários", loc = "left")
+    st.pyplot()
+else:
+    sns.scatterplot(x="Speed", y="Attack", data=pokemon, hue="Legendary", palette=pallete_pok)
+    plt.title("Ataque x Defesa\nseparado por lendários", loc="left")
+    st.pyplot()
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.deck_gl_chart(
+     viewport={
+         'latitude': 37.76,
+         'longitude': -122.4,
+         'zoom': 11,
+         'pitch': 50,
+     },
+     layers=[{
+         'type': 'HexagonLayer',
+         'data': df,
+         'radius': 200,
+         'elevationScale': 4,
+         'elevationRange': [0, 1000],
+         'pickable': True,
+         'extruded': True,
+     }, {
+         'type': 'ScatterplotLayer',
+         'data': df,
+     }])
+
+genre = st.radio(
+     "What's your favorite movie genre",
+     ('Comedy', 'Drama', 'Documentary'))
+
+if genre == 'Comedy':
+     st.write('You selected comedy.')
+else:
+     st.write("You didn't select comedy.")
+
+
+agree = st.checkbox('I agree')
+
+if agree:
+    st.write('Great!')
